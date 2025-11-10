0bcd34e5e9f6abc5bc326c0cb2623cbf0f329716
Thaysa Lacerda <t_thaysa.lacerda@hapvida.com.br>
2025-11-10 00:16:59 -0400

    fix: Ajuste nas views para padronização - Padronizando a tela de listagem de categorias - Criar dados iniciais de categorias

---
+++ src/main/resources/db/migration/V4__insertDefaultCategories.sql
@@ -0,0 +1,9 @@
+INSERT INTO Category (name, code, color, `order`, createdAt) VALUES
+-- ('Escola_PROGRAMAÇÃO', 'prog', '#00C86F', 1, NOW()),
+('Escola_FRONT-END', 'front', '#19D1D5', 2, NOW()),
+('Escola_DATA SCIENCE', 'data', '#BEEE2A', 3, NOW()),
+('Escola_INTELIGÊNCIA ARTIFICIAL', 'ia', '#A742EA', 4, NOW()),
+('Escola_DEVOPS', 'devops', '#FF6A6A', 5, NOW()),
+('Escola_UX & DESIGN', 'ux', '#FF6FCF', 6, NOW()),
+('Escola_MOBILE', 'mobile', '#FFBA05', 7, NOW()),
+('Escola_INOVAÇÃO & GESTÃO', 'gestao', '#FF8C2A', 8, NOW());
